e08a751a8d609b917adf58c100a251649aef2339
Ali Hakan <ozen.alihakan@gmail.com>
2023-03-28 21:12:18 +0300

    #5 Spring Initializr

---
+++ src/test/java/com/SWE/LivingHistory/LivingHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SWE.LivingHistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivingHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
