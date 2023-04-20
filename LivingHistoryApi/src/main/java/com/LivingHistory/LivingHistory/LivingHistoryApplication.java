b6b32a33dd991d751c3e4d3d9a258ac51ce6227a
Ali Hakan <ozen.alihakan@gmail.com>
2023-04-20 18:20:54 +0300

    Spring Boot project is initialized.

---
+++ LivingHistoryApi/src/main/java/com/LivingHistory/LivingHistory/LivingHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.LivingHistory.LivingHistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivingHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivingHistoryApplication.class, args);
+	}
+
+}
